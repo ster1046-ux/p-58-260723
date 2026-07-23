c199dc860998db889f580f746ca1e24ca88d9168
ster1046-ux <ster1046@gmail.com>
2026-07-23 11:17:02 +0900

    step3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -11,19 +14,41 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String saying = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
 
-            } else if(cmd.equals("종료")) {
-                break;
+                    lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
+
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "종료" -> {
+                    return;
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+                }
             }
         }
+
     }
 }
